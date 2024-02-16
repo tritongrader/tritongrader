2d6a553a91458aefb092212833d0323bf9f9877f
Yuchen Jing <y5jing@ucsd.edu>
2024-02-16 14:52:18 -0800

    Fix: infinite output causes OOM

--- src/tritongrader/utils.py
+++ src/tritongrader/utils.py
@@ -1,36 +1,47 @@
 import os
-import json
 import subprocess
 
 
 def get_countable_unit_string(count: int, unit: str):
-    ret = f"{count} {unit}"
-    if count != 1:
-        ret += "s"
-    return ret
+	ret = f"{count} {unit}"
+	if count != 1:
+		ret += "s"
+	return ret
 
 
 def run(
-    command,
-    capture_output=False,
-    print_command=False,
-    print_output=False,
-    timeout=None,
-    text=True,
-    arm=False,
+	command,
+	capture_output=False,
+	print_command=False,
+	print_output=False,
+	timeout=None,
+	text=True,
+	arm=False,
 ):
-    if arm:
-        command = "qemu-arm -L /usr/arm-linux-gnueabihf/ " + command
-    sp = subprocess.run(
-        command,
-        shell=True,
-        capture_output=capture_output or print_output,
-        text=text or print_output,
-        timeout=timeout,
-    )
-    if print_command:
-        print(f"$ {command}")
-    if print_output:
-        print(str(sp.stdout))
-        print(str(sp.stderr))
-    return sp
+	with open(".cmd-stdout", "wb") as fout, open(".cmd-stderr", "wb") as ferr:
+		if arm:
+			command = "qemu-arm -L /usr/arm-linux-gnueabihf/ " + command
+		sp = subprocess.run(
+			command,
+			shell=True,
+			stdout=fout,
+			stderr=ferr,
+			text=text or print_output,
+			timeout=timeout,
+		)
+	# TODO configurable max size
+	if os.path.getsize(".cmd-stdout") > 100000000 or os.path.getsize(".cmd-stderr") > 100000000:
+		# TODO unhack and properly return error
+		sp.stdout = "stdout too big"
+		sp.stderr = "stderr too big"
+	else:
+		with open(".cmd-stdout", "rb") as fout, open(".cmd-stderr", "rb") as ferr:
+			sp.stdout = str(fout.read())
+			sp.stderr = str(fout.read())
+
+	if print_command:
+		print(f"$ {command}")
+	if print_output:
+		print(str(sp.stdout))
+		print(str(sp.stderr))
+	return sp
